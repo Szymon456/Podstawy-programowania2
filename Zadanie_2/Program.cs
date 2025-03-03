274df546d753eddc7dae2647d693afacaf324d70
Szymon456 <szymi190@o2.pl>
2025-03-03 16:36:45 +0100

    Koniec zajęć

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie;
+            Console.WriteLine("Podaj swoje imie: ");
+            imie = Console.ReadLine();
+            Console.WriteLine("Witaj {0}!", imie);
+            Console.ReadKey(true);//pauza
+        }
+    }
+}
---
+++ Zadanie_4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("Podaj a=");
+            a =Convert.ToDouble(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Pole={0}", a*b);
+            Console.WriteLine("Obwód={0}", 2 * a + b * 2);
+            Console.ReadKey(true);
+        }
+    }
+}
