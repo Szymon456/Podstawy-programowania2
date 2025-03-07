18b9b30dfd4955ef02ff0c0996ed4e444687ff93
Szymon456 <szymi190@o2.pl>
2025-03-07 14:31:27 +0100

    koniec

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, h, l,V,Pc;
+            Console.Write("Podaj r=");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Podaj h=");
+            h = Convert.ToDouble(Console.ReadLine());
+            l = Math.Sqrt(r * r + h * h);
+            V = 1.0 / 3 * Math.PI * r * r * h;
+            Pc = Math.PI * r * r * h+Math.PI*r*l;
+            Console.WriteLine("V={0}",V);
+            Console.WriteLine("Pc={0}",Pc);
+            Console.ReadKey(true);//pauza
+        }
+    }
+}
---
+++ Zadanie 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Podaj a=");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}/{1}={2}r{3}", a, b, a / b, a%b);
+            Console.ReadKey(true);
+
+        }
+    }
+}
