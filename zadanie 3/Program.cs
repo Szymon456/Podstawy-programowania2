bdd7c41441245d8fede737f36eec162a3aaa9d4e
dnieduzak <d.nieduzak.058@studms.ug.edu.pl>
2025-03-17 15:42:22 +0100

    zrobione

---
+++ zadanie 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, suma = 0;
+            Console.Write("Podaj n: ");
+            n= Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= n; i++)
+                suma += i;
+            Console.WriteLine("1+...+{0}={1}", n, suma);
+            Console.ReadKey(true);
+        }
+    }
+}
