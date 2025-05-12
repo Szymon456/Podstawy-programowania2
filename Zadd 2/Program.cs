75bc06092e84c44c699bf32dc741ac13b710778a
aszewczykk <a.szewczyk.310@studms.ug.edu.pl>
2025-05-12 14:33:15 +0200

    skończone

---
+++ Zadd 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadd_2
+{
+    internal class Program
+    {
+        static long fib(long n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do obliczenia n-tego wyrazu ciągu Fibonacciego:");
+            long n = long.Parse(Console.ReadLine());
+            long wynik = fib(n);
+            Console.WriteLine($"n-ty wyraz ciągu Fibonacciego wynosi {wynik}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zad 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_1
+{
+    internal class Program
+    {
+        static long silnia(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+            else
+                return n * silnia(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do obliczenia silni:");
+            int n = int.Parse(Console.ReadLine());
+            long wynik = silnia(n);
+            Console.WriteLine($"Silnia z {n} wynosi {wynik}");
+            Console.ReadKey();
+        }
+    }
+}
+
