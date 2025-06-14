711312a95d449fd4358cbbd4511bd8d58203f566
Szymon456 <szymi190@o2.pl>
2025-06-14 16:03:44 +0200

    git

---
+++ zadddd 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadddd_1
+{
+    internal class Program
+    {
+        static int suma(int a, int b)
+        {
+            return a + b;
+        }
+
+      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2+5={0}", suma(2, 5));
+            Console.ReadKey(true); 
+    }
+}
